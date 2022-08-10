4cbbfd2fe40ddb81671851059847b9cd6c1c7bee
Manoel <manoel.lobo@gmail.com>
2022-08-10 17:03:12 -0300

    passing reference to function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-fn largest(list: Vec<i32>) -> i32 {
+fn largest(list: &[i32]) -> i32 {
     let mut largest = list[0];
 
-    for number in list {
+    for &number in list {
         if number > largest {
             largest = number;
         }
@@ -13,11 +13,11 @@ fn largest(list: Vec<i32>) -> i32 {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let result = largest(number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 }
